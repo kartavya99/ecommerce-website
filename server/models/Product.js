a17cfbf1de73e2b5678f388a9505d1b82933046b
kartavya <kartavyas@gmail.com>
2022-07-25 22:05:36 +1000

    added Product model

---
+++ server/models/Product.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0.09,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
