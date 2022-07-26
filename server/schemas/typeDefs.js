6c5fb7d6506b9c0f528e274c86a3d5548a8167a7
kartavya <kartavyas@gmail.com>
2022-07-26 15:13:27 +1000

    packages and typeDefs added

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,82 @@
+const { gql } = require("apollo-server-express");
+
+const typesDefs = gql`
+  type User {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    isAdmin: Boolean!
+    orders: [Order]
+  }
+
+  type Product {
+    _id: ID
+    productName: String!
+    image: String!
+    brand: String!
+    description: String!
+    price: Int!
+    countInStock: Int!
+  }
+
+  type Order {
+    orderItems: [OrderItem]
+  }
+
+  type OrderItem {
+    productName: String!
+    quantity: Int!
+    image: String!
+    price: Int!
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    user: User
+    product(_id: ID!): Product
+    order(_id: ID!): Order
+    allUsers(isAdmin: true): User
+  }
+
+  type Mutation {
+    createUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      isAdmin: Boolean!
+      password: String!
+    ): Auth
+    login(email: String!, password: String!): Auth
+    createProduct(
+      user: ID!
+      productName: String!
+      image: String!
+      brand: String!
+      description: String!
+      price: Int!
+      countInStock: Int!
+    ): Product
+    createOrder(product: [ID]!): Order
+    updateUser(
+        firstName: String
+        lastNameL String
+        email: String
+    ): User
+    updateProduct(
+        _id: ID!
+        productName: String
+        image: String
+         brand: String
+        description: String
+        price: Int
+        countInStock: Int
+    ): Product
+  }
+`;
+
+module.exports = typeDefs;
