5c4c149c2ab102fcc4feaad05089720bb783f1dd
kartavya <kartavyas@gmail.com>
2022-07-29 15:12:35 +1000

    client side authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,57 @@
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Products from "./components/Product/Product";
 import Footer from "./components/Footer/Footer";
 import Login from "./pages/login";
 import HomePage from "./pages/homePage";
 
+//Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+//Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage it it exists
+  const token = localStorage.getItem("id_token");
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authlink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/products" element={<Products />}></Route>
+          </Routes>
+          <Footer />
+        </Router>
+      </ApolloProvider>
     </>
   );
 }
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Products from "../components/Product/Product";
 
-const HomePage = () => {
-  return <h1> WELCOME TO ECOMM WORLD </h1>;
+const HomePage = (props) => {
+  console.log();
+  return (
+    <>
+      <h1> WELCOME TO ECOMM WORLD </h1>
+      {/* {Products.map(() => console.log("Hello"))} */}
+    </>
+  );
 };
 
 export default HomePage;
