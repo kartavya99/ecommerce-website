d07e2eb59d03a46ff06b613ee5b0791b550bb69f
kartavya <kartavyas@gmail.com>
2022-08-01 20:17:41 +1000

    login and register page styles updated

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -7,34 +7,49 @@ import classes from "./login.module.css";
 const Login = () => {
   return (
     <FormContainer>
-      <h1>Sign In</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <div className={classes["main-container"]}>
+        <div className={classes.heading}>
+          <h1>Sign In</h1>
+        </div>
+        <Form>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email address"
+              className={classes.holder}
+            />
+            <div children={classes.text}>
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </div>
+          </Form.Group>
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className={classes.holder}
+            />
+          </Form.Group>
+          <div className={classes.button}>
+            <Button type="submit" variant="dark">
+              Sign In
+            </Button>
+          </div>
+        </Form>
 
-        <Button type="submit" variant="primary">
-          Sign In
-        </Button>
-      </Form>
-
-      <Row className="py-3">
-        <Col>
-          New Customer?
-          <Link className="btn btn-dark m-4" to="/register">
-            Register
-          </Link>
-        </Col>
-      </Row>
+        <Row className="py-3">
+          <Col>
+            New Customer?
+            <Link className={classes.register} to="/register">
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </div>
     </FormContainer>
   );
 };
--- client/src/pages/register/register.js
+++ client/src/pages/register/register.js
@@ -2,55 +2,84 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../../components/Form/FormContainer";
+import classes from "./register.module.css";
 
 const registerPage = () => {
   return (
     <FormContainer>
-      <Form>
-        <Form.Group controlId="name">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="name" placeholder="First Name"></Form.Control>
-        </Form.Group>
+      <div className={classes["main-container"]}>
+        <Form>
+          <div className={classes.title}>
+            <Form.Group controlId="name">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="First Name"
+                className={classes.holder}
+              ></Form.Control>
+            </Form.Group>
+          </div>
 
-        <Form.Group controlId="name">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="name" placeholder="Last Name"></Form.Control>
-        </Form.Group>
+          <div className={classes.title}>
+            <Form.Group controlId="name">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Last Name"
+                className={classes.holder}
+              ></Form.Control>
+            </Form.Group>
+          </div>
 
-        <Form.Group controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email"></Form.Control>
-        </Form.Group>
+          <div className={classes.title}>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                className={classes.holder}
+              ></Form.Control>
+            </Form.Group>
+          </div>
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-          ></Form.Control>
-        </Form.Group>
+          <div className={classes.title}>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                className={classes.holder}
+              ></Form.Control>
+            </Form.Group>
+          </div>
 
-        <Form.Group controlId="confirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm password"
-          ></Form.Control>
-        </Form.Group>
+          <div className={classes.title}>
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                className={classes.holder}
+              ></Form.Control>
+            </Form.Group>
+          </div>
 
-        <Button type="submit" variant="primary">
-          Register
-        </Button>
-      </Form>
+          <div>
+            <Button type="submit" variant="dark">
+              Register
+            </Button>
+          </div>
+        </Form>
 
-      <Row className="py-3">
-        <Col>
-          Have an Account?{" "}
-          <Link className="btn btn-dark m-4" to="/login">
-            Login{" "}
-          </Link>
-        </Col>
-      </Row>
+        <Row className="py-3">
+          <Col>
+            Have an Account?{" "}
+            <Link className={classes.register} to="/Login">
+              Login{" "}
+            </Link>
+          </Col>
+        </Row>
+      </div>
     </FormContainer>
   );
 };
