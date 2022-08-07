57d2bdb34d2126924bd15773201b50d1305a2fc8
kartavya <kartavyas@gmail.com>
2022-08-07 13:13:23 +1000

    updated create product page

--- client/src/pages/createProductPage/CreateProductPage.js
+++ client/src/pages/createProductPage/CreateProductPage.js
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../../components/Form/FormContainer";
 import classes from "./CreateProductPage.module.css";
+import { useMutation } from "@apollo/client";
+import { CREATE_PRODUCT } from "../../utils/mutation";
+import Loader from "../../components/Loader/Loader";
 
 const CreateProductPage = () => {
+  const [formState, setFormState] = useState({
+    user: "",
+    productName: "",
+    image: "",
+    brand: "",
+    description: "",
+    price: "",
+    countInStock: "",
+    uploading: "false",
+  });
+
+  const [createProduct, { error }] = useMutation(CREATE_PRODUCT);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await createProduct({
+        variables: {
+          user: formState.user,
+          productName: formState.productName,
+          image: formState.image,
+          brand: formState.brand,
+          description: formState.description,
+          price: formState.price,
+          countInStock: formState.countInStock,
+          uploading: formState.uploading,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // const handleChangeInt = (event) => {
+  //   const { name, value } = parseFloat(event.target);
+  //   console.log(event.target);
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
+
+  const uploadFileHandler = async (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    const formData = new FormData();
+    formData.append("image", file);
+    formState.uploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      formState.image(data);
+      formState.uploading(false);
+    } catch (error) {
+      console.error(error);
+      formState.image(false);
+    }
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.link}>
@@ -14,21 +93,26 @@ const CreateProductPage = () => {
       </div>
       <FormContainer>
         <p className={classes.heading}>EDIT PRODUCT</p>
-        <Form>
+        <Form onSubmit={handleFormSubmit}>
           <Form.Group>
             <Form.Label htmlFor="productName">Product Name</Form.Label>
             <Form.Control
-              type="productName"
+              type="text"
               id="productName"
+              name="productName"
               placeholder="Enter Product Name"
+              onChange={handleChange}
             ></Form.Control>
           </Form.Group>
+
           <Form.Group>
             <Form.Label htmlFor="price">Product price</Form.Label>
             <Form.Control
               type="number"
               id="price"
+              name="price"
               placeholder="Enter Product Price"
+              onChange={handleChange}
             ></Form.Control>
           </Form.Group>
           <Form.Group>
@@ -36,16 +120,34 @@ const CreateProductPage = () => {
             <Form.Control
               type="text"
               id="image"
+              name="image"
               placeholder="Enter image"
+              onChange={uploadFileHandler}
             ></Form.Control>
-            {/* <Form.File id="image-file" label="Choose File" custom></Form.File> */}
+            {/* {formState.uploading && <Loader />} */}
+
+            <input
+              type="file"
+              className="custom-file-input"
+              id="inputGroupFile01"
+            />
+
+            {/* <Form.File
+              id="image-file"
+              label="Choose File"
+              custom
+              onChange={handleChange}
+            ></Form.File> */}
           </Form.Group>
+          <br />
           <Form.Group>
             <Form.Label htmlFor="brand">Brand</Form.Label>
             <Form.Control
               type="text"
               id="brand"
+              name="brand"
               placeholder="Enter Brand Name"
+              onChange={handleChange}
             ></Form.Control>
           </Form.Group>
           <Form.Group>
@@ -53,7 +155,9 @@ const CreateProductPage = () => {
             <Form.Control
               type="number"
               id="countInStock"
+              name="countInStock"
               placeholder="Enter stock count"
+              onChange={handleChange}
             ></Form.Control>
           </Form.Group>
           <Form.Group>
@@ -61,9 +165,28 @@ const CreateProductPage = () => {
             <Form.Control
               type="text"
               id="description"
+              name="description"
               placeholder="Enter product description"
+              onChange={handleChange}
             ></Form.Control>
           </Form.Group>
+          {/* <Form.Group>
+            <Form.Label htmlFor="user">User Name</Form.Label>
+            <Form.Control
+              type="text"
+              id="user"
+              name="user"
+              placeholder="Enter User Name"
+              onChange={handleChange}
+            ></Form.Control>
+          </Form.Group> */}
+
+          {error ? (
+            <div>
+              <p className="error-text">Please provided all the details</p>
+            </div>
+          ) : null}
+
           <Button
             type="submit"
             variant="dark"
